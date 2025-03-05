ababa1e055ea6e8d35800757a45555f35908fff8
conno <charling@calpoly.edu>
2025-03-04 16:23:51 -0800

    yayy

--- race.py
+++ race.py
@@ -38,3 +38,40 @@ def chooseCarOne():
     return newCar
 
 chooseCarOne()
+
+def chooseCarTwo():
+    make=''
+    model=''
+    year=''
+    acceleration=''
+    topSpeed=''
+    with open("text.txt", "r") as choice:
+        choices=choice.read()
+        line=input(choices)
+    with open("text.txt","r") as lst:
+        l=int(line)
+        g=0
+        while g<l:
+            g+=1
+            r=lst.readline()
+        #f=lst.readlines()
+        car=r
+    cars = car.split()
+    carDict={}
+    for i in range(len(cars)):
+        if cars[i]=='Make:':
+            carDict['Make']=cars[i+1]
+
+        if cars[i]=='Model:':
+            carDict['Model']=cars[i+1]
+
+        if cars[i]=='Year:':
+            yea=cars[i+1]
+        if cars[i]=='Acceleration:':
+            acce=cars[i+1]
+        if cars[i]=='TopSpeed:':
+            ts=cars[i+1]
+    newCar=Car(carDict.get('Make'),carDict.get('Model'),year,acceleration,topSpeed)
+    #print(type(newCar))
+    newCar.display_info()
+    return newCar
